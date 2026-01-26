f6ded0a24b5c09c309cdd1f92cc4129d59b8a545
padredemellis <10656@holbertonstudents.com>
2026-01-26 11:22:11 -0300

    Add task1 - Flask server in Docker

--- task1/api.py
+++ task1/api.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/api/hello')
 def hello_world():
+    """
+    Esta ruta devuelve un saludo
+    """
     return 'Hello, World!'
 
 if __name__ == '__main__':
+
+
     app.run(host='0.0.0.0', port=5252)
